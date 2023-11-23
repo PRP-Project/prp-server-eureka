2b42f3a3b03b65a412a330a04d0819e046305070
yoonwh <onetruth203@gmail.com>
2023-11-23 21:57:00 +0900

    Feat-Add-20231123, 유레카 서버 로ì로컬 버전 추가

---
+++ src/main/java/com/prp/server/eureka/PrpServerEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.prp.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PrpServerEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrpServerEurekaApplication.class, args);
+    }
+
+}
